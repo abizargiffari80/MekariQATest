eb4754bd960f29d6ffb04cf531da5f336c07484f
Abizar Giffari <abizargiffari80@gmail.com>
2020-11-20 15:56:13 +0700

    Final test case

--- features/step_definitions/sales_invoice.rb
+++ features/step_definitions/sales_invoice.rb
@@ -7,21 +7,36 @@ Given("Login and go menu sales") do
   
   Then("Menu sales") do
     page.should have_xpath("//span[contains(text(),'Sales')]")
-    find(:xpath, "//a[@id='vnav-sales-link']").click
-    find(:xpath, "//span[contains(text(),'Create New Sale')]").click
-    sleep 3
-    find(:xpath, "//header/div[1]/div[2]/div[1]/div[1]/ul[1]/li[1]/a[1]").click
-    sleep 3
   end
   
   When("Making sales invoice") do
-    
+    find(:xpath, "//a[@id='vnav-sales-link']").click
+    find(:xpath, "//span[contains(text(),'Create New Sale')]").click
+    find(:xpath, "//header/div[1]/div[2]/div[1]/div[1]/ul[1]/li[1]/a[1]").click
+    find(:xpath, "//span[@id='select2-chosen-19']").click
+    find(:xpath, "//div[@id='select2-result-label-0']").click
+    #note = please change Abi testN+1 after running 1 time
+    #example Abi test24, Abi test25
+    #because once it run, Abi test24 will be already saved and program cant run properly
+    find(:xpath, "//input[@id='person_display_name']").fill_in with: 'Abi test24'
+    find(:xpath, "//button[@id='aodc-add']").click
+    sleep 3
+    find(:xpath, "//input[@id='transaction_transaction_date']").fill_in with: '01/11/2020'
+    find(:xpath, "//span[@id='select2-chosen-20']").click
+    find(:xpath, "//div[@id='select2-result-label-0']").click
+    #note = please change MotorN+1 after running 1 time
+    #example 'Motor24' then run, 'Motor25' then run
+    #because once it run, 'Motor24' will be already saved and program cant run properly
+    find(:xpath, "//input[@id='product_name']").fill_in with: 'Motor24'
+    find(:xpath, "//button[@id='add-new-product']").click
+    sleep 3
+    find(:xpath, "//input[@id='transaction_transaction_lines_attributes_0_rate']").fill_in with: '100000'
+    find(:xpath, "//input[@id='transaction_transaction_lines_attributes_0_rate']").send_keys :tab
+    find(:xpath, "//input[@id='create_button']").click
   end
   
-  Then("Sales invoice success") do
-    
-  end
-
-  # upload file
-  # attach_file('in put[type='file']', File.absolute_path('./fileset/publisher/upload_pic.jpg'))
-  # find(:xpath, "input[type='file']").send_keys (lokasi file)
+  
+Then("Sales invoice success") do
+  page.should have_css('div.flash-message.alert.alert-success')
+  #up untill this i honestly dont know why invoice didn't create succesfully 
+end
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,8 @@ require 'pry'
 require 'capybara/cucumber'
 require 'capybara/rspec'
 require 'rspec/expectations'
+require "timeout"
 
     Capybara.default_driver=:selenium_chrome
     Capybara.app_host='https://sandbox.jurnal.id/'
+    Capybara.page.driver.browser.manage.window.maximize
